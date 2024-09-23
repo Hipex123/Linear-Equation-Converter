e3b9f34212c247548741f78282ec7367b8aa47cf
Hipex123 <117768734+Hipex123@users.noreply.github.com>
2024-09-23 22:27:55 +0200

    Set present mode to FIFO
    Before this: app has been using 97% of my 3D rendering power on my GTX 1650

--- src/main.cpp
+++ src/main.cpp
@@ -1514,14 +1514,6 @@ private:
 
     VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes)
     {
-        for (const auto& availablePresentMode : availablePresentModes)
-        {
-            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
-            {
-                return availablePresentMode;
-            }
-        }
-
         return VK_PRESENT_MODE_FIFO_KHR;
     }
 
